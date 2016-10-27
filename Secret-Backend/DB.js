c87e23a04c7769ba7501fc8036e8906a22e2a95d
manoharprabhu <manoharagent13@gmail.com>
2016-10-28 00:37:34 +0530

    Initial backend commit

---
+++ Secret-Backend/DB.js
@@ -0,0 +1,81 @@
+var Database = (function() {
+    var MockData = require("./mockdata");
+    var DataStore = require("nedb");
+    var db = new DataStore({filename: "secrets", autoload: true});
+    db.ensureIndex({
+        fieldName: "secret"
+    }, function(err) {
+        if (err) {
+            console.log("Error while creating index on the secret attribute");
+        } else {
+            console.log("Attribute 'secret' indexed successfully");
+        }
+    });
+
+    function getRandomInt(limit) {
+        return Math.floor(Math.random() * 100000) % limit;
+    }
+
+    RegExp.escape = function(s) {
+        return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+    };
+
+    var getRandomSecret = function(callback) {
+        db.count({}, function(err, count) {
+            if (err) {
+                callback(err);
+                return;
+            }
+            if (count === 0) {
+                callback(null, []);
+            }
+            var randomSkip = getRandomInt(count);
+            db.findOne({}).skip(randomSkip).exec(function(err, doc) {
+                if (err) {
+                    callback(err);
+                } else {
+                    callback(null, doc);
+                }
+            });
+        });
+    };
+
+    var getSecretWithKeyWord = function(num, keyword, callback) {
+        db.find({
+            "secret": new RegExp(RegExp.escape(keyword))
+        }).limit(num).exec(function(err, docs) {
+            if (err) {
+                callback(err);
+            } else {
+                callback(null, docs);
+            }
+        });
+    };
+
+    var getTopTrendingSecret = function(num, callback) {
+        db.find({}).sort({"likes": -1, "dislikes": 1}).limit(num).exec(function(err, docs) {
+            if (err) {
+                callback(err);
+            } else {
+                callback(null, docs);
+            }
+        });
+    };
+
+    var postNewSecret = function(secretText, callback) {
+        var document = {
+            "secret": secretText,
+            "comments": [],
+            "likes": 0,
+            "dislikes": 0
+        };
+        db.insert(document, function(err, newDocs) {
+            callback(err);
+        });
+    };
+
+    return {getRandomSecret, getSecretWithKeyWord, getTopTrendingSecret, postNewSecret}
+
+}());
+
+module.exports = Database;
---
+++ Secret-Backend/app.js
@@ -0,0 +1,63 @@
+var express = require("express");
+var DB = require("./DB");
+var app = express();
+var path = require("path");
+var bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+var PORT = process.env.PORT || 8080;
+
+/*
+ Get a random secret as per the given criteria.
+ Get random secret if no criteria is passed.
+ Search the secrets and return the top X secrets having the keyword if passes.
+*/
+app.get("/secret", function(req, res) {
+    var keyword = req.query.keyword;
+    if (keyword) {
+        console.log("keyword found " + keyword);
+        //Search for the keyword in the secrets
+        DB.getSecretWithKeyWord(10, keyword, function(error, secrets) {
+            if (error) {
+                res.json({"error": true, "message": error});
+            } else {
+                res.json(secrets);
+            }
+        });
+    } else {
+        //Get a random secret
+        DB.getRandomSecret(function(error, secret) {
+            if (error) {
+                res.json({"error": true, "message": error});
+            } else {
+                res.json(secret);
+            }
+        });
+    }
+});
+
+app.get("/trending", function(req, res) {
+    DB.getTopTrendingSecret(10, function(err, data) {
+        if (err) {
+            res.json({"error": true, "message": error});
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+// Post a new secret. Store the secret text and initialize the comments and likes/dislikes to 0
+app.post("/newsecret", function(req, res) {
+    var secretText = req.body.secret;
+    DB.postNewSecret(secretText, function(error) {
+        if (error) {
+            res.json({"error": true, "message": error});
+        } else {
+            res.json({"success": true});
+        }
+    })
+});
+
+app.listen(PORT, function() {
+    console.log("Server started at port " + PORT);
+});
